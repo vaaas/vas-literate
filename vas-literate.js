f870611b8d2a9f03827891024e086fc37cc439ad
Vasileios Pasialiokis <pasivasi@outlook.com>
2021-11-10 14:02:26 +0200

    add jshint and enable writing for single files

--- vas-literate.js
+++ vas-literate.js
@@ -63,46 +63,50 @@ function process_block(x) {
 }
 
 function process_file(file, root, dest) {
-    return (
-        reduce(
-            (xs, x) => {
-                const k = first(x) ? path.join(dest, first(x)) : guess_file_name(file.pathname).replace(root, dest)
-                if (xs[k] === undefined) xs[k] = { mtime: 0, blocks: [] }
-                xs[k].blocks.push(second(x))
-					 xs[k].mtime = Math.max(xs[k].mtime, file.mtime)
-                return xs
-            }, {},
+    return reduce(
+        (xs, x) => {
+            const k = first(x) ?
+                path.join(dest, first(x)) :
+                guess_file_name(file.pathname).replace(root, dest)
+            if (xs[k] === undefined) xs[k] = { mtime: 0, blocks: [] }
+            xs[k].blocks.push(second(x))
+                 xs[k].mtime = Math.max(xs[k].mtime, file.mtime)
+            return xs
+        }, {},
         map(process_block,
         block_generator(
         fs.readFileSync(file.pathname, { encoding: 'utf-8' }))))
-    )
 }
 
 function main(root='literate', dest='src') {
     const now = Date.now()
 
-    const files = (
-        reduce(
-            (files, x) => {
-                for (const [k, v] of Object.entries(x))
-                    files[k] = v
-                return files
-            }, {},
-        map(x => process_file(x, root, dest),
-        walk_file_directory(root)))
-    )
+    const stats = fs.statSync(root)
+    const files = reduce(
+        (files, x) => {
+            for (const [k, v] of Object.entries(x))
+                files[k] = v
+            return files
+        }, {},
+        stats.isFile() ?
+            [process_file({ pathname: './' + root, mtime: stats.mtime }, '.', dest)] :
+            map(x => process_file(x, root, dest), walk_file_directory(root)))
 
-    Object.keys(files).map(path.dirname).forEach(x => fs.mkdirSync(x, { recursive: true }))
-    Object.entries(files).forEach(([k, v]) => {
-        var mtime
-        try { mtime = fs.statSync(k).mtime.getTime() }
-        catch(e) { mtime = 0 }
-        if (v.mtime > mtime) {
-            console.error(k, ' was updated, overwriting')
-            fs.writeFileSync(k, v.blocks.join('\n'))
-        } else
-            console.error(k, ' was not updated, so not writing')
-    })
+    Object.keys(files)
+        .map(path.dirname)
+        .forEach(x => fs.mkdirSync(x, { recursive: true }))
+
+    Object.entries(files)
+        .forEach(([k, v]) => {
+            var mtime
+            try { mtime = fs.statSync(k).mtime.getTime() }
+            catch(e) { mtime = 0 }
+            if (v.mtime > mtime) {
+                console.error(k, ' was updated, overwriting')
+                fs.writeFileSync(k, v.blocks.join('\n'))
+            } else
+                console.error(k, ' was not updated, so not writing')
+        })
 
     console.log('Completed in', (Date.now() - now)/1000, 'seconds')
 }
